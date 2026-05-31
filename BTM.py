49feca4774333ac321f19220b66390990eb9bdcd
TayyibBello <kakalikadc@gmail.com>
2026-05-31 14:12:40 +0000

    Add files via upload
    On my honor, Bello Tayyib Mohammad, this programming assignment is my own work
     and I have not provided this code to any other student.

---
+++ BTM.py
@@ -0,0 +1,27 @@
+# Name: Bello Mohammad Tayyib
+# Student ID: 2425402568
+# Assignment: Initials
+# File: BTM.py
+
+
+def main():
+    # Prints the initials in the required stylized format.
+    print("")
+    print("...")
+    print("BMT")
+    print("")
+    print("...")
+    print("BBBBBBBBBB..........MM........MM........TTTTTTTTTTTT........")
+    print("BB........BB........MMM......MMM............TTTT............")
+    print("BB........BB........MMMM....MMMM............TTTT............")
+    print("BB........BB........MM.MM..MM.MM............TTTT............")
+    print("BBBBBBBBBB..........MM..MMMM..MM............TTTT............")
+    print("BB........BB........MM...MM...MM............TTTT............")
+    print("BB........BB........MM........MM............TTTT............")
+    print("BB........BB........MM........MM............TTTT............")
+    print("BB........BB...**...MM........MM...**.......TTTT.......**...")
+    print("BBBBBBBBBB.....**...MM........MM...**.......TTTT.......**...")
+    print("")
+
+
+main()
